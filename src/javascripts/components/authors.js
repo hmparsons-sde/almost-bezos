4a97adfb288b2a8bb92205f35a3f5930bd13f815
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-03-02 14:53:10 -0600

    debug

--- src/javascripts/components/authors.js
+++ src/javascripts/components/authors.js
@@ -1,18 +1,20 @@
 const showAuthors = (array) => {
   document.querySelector('#store').innerHTML = '';
-  document.querySelector('#add-button').innerHTML = '<button class="btn btn-success btn-lg mb-4" id="add-author-btn">Add An Author</button>';
-
   document.querySelector('#form-container').innerHTML = '';
-
+  // CREATE A BUTTON TO ADD BOOKS
+  document.querySelector('#add-button').innerHTML = '<button class="btn btn-success btn-lg mb-4" id="add-author-btn">Add An Author</button>';
   array.forEach((item) => {
     // FIXME: STUDENTS create cards for your authors
     document.querySelector('#store').innerHTML += `
-    <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">${item.first_name} ${item.last_name}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">${item.email}</h6>
-      </div>
-    </div>`;
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${item.first_name} ${item.last_name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${item.email}</h6>
+    <p class="card-text">${item.first_name} is an independent artist.</p>
+    <button class="btn btn-info" data-toggle="modal" data-target="#formModal" id="edit-author-btn--${item.firebaseKey}">Edit Author</button>
+    <button class="btn btn-danger" id="delete-author--${item.firebaseKey}">Delete Author</button>
+  </div>
+</div>`;
   });
 };
 
